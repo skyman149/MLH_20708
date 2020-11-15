887415f44ea8d37b2d2ef819e16a21ac16d8e7a8
roma09877 <romanfedunvlad@gmail.com>
2020-11-14 18:10:00 -0800

    Add storyType test cases

--- wdio.conf.js
+++ wdio.conf.js
@@ -17,7 +17,8 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        './test/smoke/inputs.js'
+        './test/smoke/inputs.js',
+
     ],
     // Patterns to exclude.
     exclude: [
